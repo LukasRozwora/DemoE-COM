8704f0e3955cf4338a06de3ff97f949f1c98ede8
LukasRozwora <lukasz.rozwora@fotc.com>
2026-06-14 19:39:37 +0200

    Add image URL for Milan in script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const products = [
     { id: 9, name: 'Sevilla', price: 84.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_wigwi0wigwi0wigw%20(1).png' },
     { id: 10, name: 'Valencia', price: 74.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_2xs8mu2xs8mu2xs8.png' },
     { id: 11, name: 'Turyn', price: 84.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_frr1vcfrr1vcfrr1.png' },
-    { id: 12, name: 'Milan', price: 89.99, image: '' },
+    { id: 12, name: 'Milan', price: 89.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_5h6gd05h6gd05h6g.png' },
     { id: 13, name: 'Rimini', price: 79.99, image: '' },
     { id: 14, name: 'Roma', price: 94.99, image: '' },
     { id: 15, name: 'Napoli', price: 64.99, image: '' },
