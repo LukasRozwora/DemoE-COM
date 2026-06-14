c41dd11c0ff22238ced59899fbafdc343d4d4902
LukasRozwora <lukasz.rozwora@fotc.com>
2026-06-14 18:27:58 +0200

    Add image URL for Madrid in script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const products = [
     { id: 4, name: 'Newcastle', price: 79.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_a9y7wia9y7wia9y7.png' },
     { id: 5, name: 'Edinburgh', price: 79.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_8tklvs8tklvs8tkl.png' },
     { id: 6, name: 'Barcelona', price: 94.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_1xudlq1xudlq1xud.png' },
-    { id: 7, name: 'Madrid', price: 94.99, image: '' },
+    { id: 7, name: 'Madrid', price: 94.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_l8bw3dl8bw3dl8bw.png' },
     { id: 8, name: 'Alicante', price: 89.99, image: 'https://raw.githubusercontent.com/LukasRozwora/DemoE-COM/main/Gemini_Generated_Image_2by4aj2by4aj2by4.png' },
     { id: 9, name: 'Sevilla', price: 84.99, image: '' },
     { id: 10, name: 'Valencia', price: 74.99, image: '' },
